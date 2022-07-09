6839e083679d297825b63895931a48e4391178d1
SoulsArchitecht <serje_fox@mail.ru>
2022-07-09 13:05:48 +0800

    projectInitWithBaseFiles

---
+++ PizzaCustomShop/src/main/java/com/sshibko/custompizza/PizzaCustomShopApplication.java
@@ -0,0 +1,13 @@
+package com.sshibko.custompizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaCustomShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaCustomShopApplication.class, args);
+	}
+
+}
